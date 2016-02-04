bb9be681cefa8863146e43c75ecf5cbab9ac833e
santos22 <santosjavier22@gmail.com>
2016-02-04 01:53:11 -0600

    Adjusted webview height - was conflicting with tab bar at the bottom

--- GrioBookChallenge/GutenbergViewController.swift
+++ GrioBookChallenge/GutenbergViewController.swift
@@ -18,14 +18,12 @@ class GutenbergViewController: UIViewController {
         let requestObj = NSURLRequest(URL: url!);
         gutenbergWebview.loadRequest(requestObj);
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
