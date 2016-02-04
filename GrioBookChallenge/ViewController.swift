d42c646839f04b27c006852a8e469d13c09b78e2
santos22 <santosjavier22@gmail.com>
2016-02-03 22:59:41 -0600

    Setting up an array of html file names in preparation for table view

--- GrioBookChallenge/ViewController.swift
+++ GrioBookChallenge/ViewController.swift
@@ -9,15 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var htmlFiles: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // iterate through directory of html files
+        let serverUrl = "http://127.0.0.1:5000/uploads/"
+        
+        // iterate through directory of html files and add to array
         let filemanager:NSFileManager = NSFileManager()
         let files = filemanager.enumeratorAtPath("/Users/santossolorzano/Desktop/challenge-books")
         while let file = files?.nextObject() {
-            print(file)
+            htmlFiles.append(file as! String)
+        }
+        
+        // remove .DS_Store file
+        htmlFiles.removeAtIndex(0) // numbers will be [2, 3]
+        
+        for file in htmlFiles {
+            print("File " + file)
         }
         // Do any additional setup after loading the view, typically from a nib.
     }
